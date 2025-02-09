91aa174509ba4f501e65b71ecec96ff340172679
Idriss Denou <idrissdenou.devops@gmail.com>
2025-02-09 10:01:16 +0000

    files completed

---
+++ secret.sh
@@ -0,0 +1,4 @@
+echo Do not track this file please
+echo git is easy and it is one module/tool of DevOps End to End automation
+date 
+echo pwd
---
+++ users.java
@@ -0,0 +1,12 @@
+#List of DevOps of Engineers at Airbus
+Idriss
+Simon
+Sylvie
+Eric
+Evolie
+Landmark
+Jessica
+Rudolf
+Chuka
+Ken
+Kennedy
